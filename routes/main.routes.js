d38388e96525ddce23624863f84ffe4a85a1cc44
RawdneyMendes <53352051+RawdneyGoncalves@users.noreply.github.com>
2022-03-09 20:11:59 -0300

    Add files via upload

---
+++ routes/main.routes.js
@@ -0,0 +1,38 @@
+const express = require("express");
+const router = express.Router();
+
+const Petshop = require("../modules/petshop");
+const Product = require("../modules/product");
+const authController = require("../controllers/auth.controller");
+router.get("/petshops", async (req, res, next) => {
+  try {
+    const petshops = await Petshop.find();
+    res.json(petshops);
+  } catch (error) {
+    res.json({ error: true, message: error.message });
+  }
+});
+router.get("/petshops/:id", async (req, res, next) => {
+  try {
+    const petshop = await Petshop.findById(req.params.id);
+
+    const product = await Product.find({ petshop_id: petshop.id });
+
+    res.json({ error: false, petshop: { ...petshop._doc, product } });
+  } catch (error) {
+    res.json({ error: true, message: error.message });
+  }
+});
+
+router.post("/purchase", async (req, res) => {
+  try {
+    const transaction = await createSplitTransaction(req.body);
+    res.json(transaction);
+  } catch (error) {
+    res.json({ error: true, message: error.message });
+  }
+});
+
+//routes.post("/login", authController.signin);
+
+module.exports = router;
